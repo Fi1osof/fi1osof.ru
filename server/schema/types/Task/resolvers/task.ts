01349fdbfedf3f251a623ec12162ffa60b31bab4
Nikolay Lanets <n.lanets@freecode.academy>
2026-06-16 07:40:19 +0300

    Fix Task::Project

--- server/schema/types/Task/resolvers/task.ts
+++ server/schema/types/Task/resolvers/task.ts
@@ -1,4 +1,5 @@
 import { builder } from '../../../builder'
+import { hasFieldInSelection } from '../../helpers/hasFieldInSelection'
 import { TaskWhereUniqueInput } from '../inputs'
 
 builder.queryField('task', (t) =>
@@ -8,7 +9,7 @@ builder.queryField('task', (t) =>
     args: {
       where: t.arg({ type: TaskWhereUniqueInput, required: true }),
     },
-    resolve: async (query, _root, args, ctx) => {
+    resolve: async (query, _root, args, ctx, info) => {
       const taskId = args.where.id
       if (!taskId) {
         throw new Error('Task ID is required')
@@ -18,6 +19,9 @@ builder.queryField('task', (t) =>
         ...query,
         where: {
           id: taskId,
+        },
+        include: {
+          Project: hasFieldInSelection(info, 'Project'),
         },
       })
     },
