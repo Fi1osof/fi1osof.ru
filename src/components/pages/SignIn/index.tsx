d76ee699ba50f6066501d40560fdf370622ee3bf
Nikolay Lanets <n.lanets@freecode.academy>
2026-06-20 12:15:32 +0300

    Fix authed redirect

--- src/components/pages/SignIn/index.tsx
+++ src/components/pages/SignIn/index.tsx
@@ -2,8 +2,11 @@ import { useRouter } from 'next/router'
 import { useCallback } from 'react'
 import { SignInForm } from 'src/components/Auth/SignInForm'
 import { SignInPageStyled } from './styles'
+import { useAuthedRedirect } from 'src/components/Auth/hooks/useAuthedRedirect'
 
 export const SignInPage: React.FC = () => {
+  useAuthedRedirect()
+
   const router = useRouter()
 
   const handleSuccess = useCallback(() => {
--- src/components/pages/SignUp/index.tsx
+++ src/components/pages/SignUp/index.tsx
@@ -2,8 +2,11 @@ import { useRouter } from 'next/router'
 import { useCallback } from 'react'
 import { SignUpForm } from 'src/components/Auth/SignUpForm'
 import { SignUpPageStyled } from './styles'
+import { useAuthedRedirect } from 'src/components/Auth/hooks/useAuthedRedirect'
 
 export const SignUpPage: React.FC = () => {
+  useAuthedRedirect()
+
   const router = useRouter()
 
   const handleSuccess = useCallback(() => {
